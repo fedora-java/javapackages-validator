69dcfece97a962fd28efcebe9e3e909d1eec23b2
Marián Konček <marian.koncek@mailbox.org>
2024-11-12 13:50:18 +0100

    Make the run script work with multiple JVM installations

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-set -eu
+set -e
 
-: ${JAVA_HOME:=/usr/lib/jvm/jre-23}
+if [ -z "${JAVA_HOME}" ]; then
+	for path in $(find /usr/lib/jvm -mindepth 1 -maxdepth 1 -type d); do
+		if [ "$(echo "${path}" | sed -E 's%/usr/lib/jvm/(java|jre)-([0-9]*).*%\2%')" -ge 22 ]; then
+			JAVA_HOME="${path}"
+			break
+		fi
+	done
+fi
 
 exec "${JAVA_HOME}"/bin/java --enable-native-access ALL-UNNAMED -jar 'target/validator.jar' "${@}"
 
